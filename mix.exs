f4e3de421357f58e0495300d6886b4771c962ef0
Leandro Ostera <leandro@ostera.io>
2020-04-21 21:57:06 +0200

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule BinBuf.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    [
+      {:rustler, "0.21.0"}
+    ]
+  end
+end
